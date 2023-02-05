94c1ed459b76c76044331305e72763229fea9d44
Enriconi <gabriel.enriconi@gmail.com>
2023-02-04 22:55:49 -0300

    chore: a time diff example

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 import Countdown from './countdown.js';
+
+const christmasDiff = new Countdown('24 December 2023 23:59:59 GMT-0300');
+
+setInterval(() => {
+  console.clear();
+  console.log(christmasDiff.total);
+}, 1000);
