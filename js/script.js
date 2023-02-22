335f9097aea9c48e681176b4e17888ee78439d70
Enriconi <gabriel.enriconi@gmail.com>
2023-02-22 05:11:23 -0300

    feat:  add input datetime

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 import Countdown from './modules/countdown.js';
 
-const timeDiff = new Countdown('10 March 2023 16:00:00 GMT-0300');
+const selectedDate = document.querySelector('#selectedDate');
 
-setInterval(() => {
-  days.innerText = timeDiff.total.days;
-  hours.innerText = timeDiff.total.hours;
-  minutes.innerText = timeDiff.total.minutes;
-  seconds.innerText = timeDiff.total.seconds;
-}, 1000);
+const changeDate = () => {
+  const timeDiff = new Countdown(new Date(selectedDate.value));
 
-const days = document.querySelector('#days');
-const hours = document.querySelector('#hours');
-const minutes = document.querySelector('#minutes');
-const seconds = document.querySelector('#seconds');
+  const days = document.querySelector('#days');
+  const hours = document.querySelector('#hours');
+  const minutes = document.querySelector('#minutes');
+  const seconds = document.querySelector('#seconds');
+
+  setInterval(() => {
+    days.innerText = timeDiff.total.days;
+    hours.innerText = timeDiff.total.hours;
+    minutes.innerText = timeDiff.total.minutes;
+    seconds.innerText = timeDiff.total.seconds;
+  }, 1000);
+};
+
+selectedDate.addEventListener('change', changeDate);
